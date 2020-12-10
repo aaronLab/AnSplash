4d2925eb3877688f5e698860bf51fdbab4209d9d
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:21:44 +0900

    feat: Add date extensions

--- AnSplash/Core/Extensions/Date+Extensions.swift
+++ AnSplash/Core/Extensions/Date+Extensions.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+extension Date {
+    
+    public var year: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    public var month: Int {
+         return Calendar.current.component(.month, from: self)
+    }
+    
+    public var day: Int {
+         return Calendar.current.component(.day, from: self)
+    }
+    
+    public var monthName: String {
+        let nameFormatter = DateFormatter()
+        nameFormatter.dateFormat = "MMMM"
+        return nameFormatter.string(from: self)
+    }
+    
+}
