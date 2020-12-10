d0a9a8055d693335a08df6fa89c8adf8da094b2b
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:22:04 +0900

    feat: Add date change helper

--- AnSplash/Core/Helpers/CommonUtils.swift
+++ AnSplash/Core/Helpers/CommonUtils.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+final class CommonUtils {
+    
+    static func unixDate2Date(string: String?) -> Date? {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        guard let string = string else { return nil }
+        let date = formatter.date(from: string)
+        return date
+    }
+    
+}
