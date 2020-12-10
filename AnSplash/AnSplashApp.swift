647eb6d9366b791b037cd6f07db4169c32dc543e
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:00:22 +0900

    Initial Commit

---
+++ AnSplash/AnSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnSplashApp.swift
+//  AnSplash
+//
+//  Created by Aaron Lee on 2020-12-10.
+//
+
+import SwiftUI
+
+@main
+struct AnSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
