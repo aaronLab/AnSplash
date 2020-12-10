69fa327438ab0f08a5f590a1c026645f75354dcb
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:26:39 +0900

    feat: Add Links model

---
+++ AnSplash/Models/Links.swift
@@ -0,0 +1,15 @@
+//
+//  Links.swift
+//  AnSplash
+//
+//  Created by Aaron Lee on 2020-12-10.
+//
+
+import Foundation
+
+struct Links: Decodable {
+    let `self`: String?
+    let html: String?
+    let download: String?
+    let download_location: String?
+}
