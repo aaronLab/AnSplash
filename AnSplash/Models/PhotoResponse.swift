aa0f6371b157d022b0947fe0eea6c84135edddde
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:36:21 +0900

    feat: Add PhotoResponse model

--- AnSplash/Models/PhotoResponse.swift
+++ AnSplash/Models/PhotoResponse.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct PhotoResponse: Decodable, Identifiable {
+    let id: String
+    let created_at: String?
+    let updated_at: String?
+    let promoted_at: String?
+    let width: Int?
+    let height: Int?
+    let color: String?
+    let description: String?
+    let urls: URLs?
+    let links: Links?
+    let likes: Int?
+    let liked_by_user: Bool?
+    let sponsorship: Sponsorship?
+    let user: User?
+}
+
+extension PhotoResponse {
+    
+    var convertedCreatedAt: Date? {
+        CommonUtils.unixDate2Date(string: self.created_at)
+    }
+    
+    var convertedUpdatedAt: Date? {
+        CommonUtils.unixDate2Date(string: self.updated_at)
+    }
+    
+    var convertedPromotedAt: Date? {
+        CommonUtils.unixDate2Date(string: self.promoted_at)
+    }
+    
+}
