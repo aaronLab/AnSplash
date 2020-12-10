fe9f8c76fbf7b6e7a648727570f06c2b9a1bba61
Aaron Lee <aaronlab.net@gmail.com>
2020-12-10 22:36:34 +0900

    feat: Add URLs model

--- AnSplash/Models/URLs.swift
+++ AnSplash/Models/URLs.swift
@@ -10,7 +10,9 @@ import Foundation
 struct URLs: Decodable {
     let raw: String?
     let full: String?
+    let large: String?
     let regular: String?
+    let medium: String?
     let small: String?
     let thumb: String?
 }
