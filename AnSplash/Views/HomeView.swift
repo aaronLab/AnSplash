a7c88af414b164a14518c7534d477f31410c1ec6
Aaron Lee <aaronlab.net@gmail.com>
2020-12-11 21:18:32 +0900

    feat: Init HomeView

---
+++ AnSplash/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  AnSplash
+//
+//  Created by Aaron Lee on 2020-12-11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
